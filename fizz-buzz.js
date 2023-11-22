5668cf5c40d6dd6c8b7953d39b92763eef7e267c
Fatih Yüksel <fatihyuksel3109@gmail.com>
2023-11-23 02:07:19 +0300

    fizzbuzz v2

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,15 +1,32 @@
 function FizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
+  }
 }
 
 FizzBuzz(10);
+
+// Short version
+
+function FizzBuzz2(n) {
+  for (let i = 1; i <= n; i++)
+    console.log(
+      i % 3 === 0 && i % 5 === 0
+        ? "FizzBuzz"
+        : i % 3 === 0
+        ? "Fizz"
+        : i % 5 === 0
+        ? "Buzz"
+        : i
+    );
+}
+
+FizzBuzz2(20);
