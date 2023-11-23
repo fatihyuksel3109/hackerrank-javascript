dad41902d56c223d23e06a75ba5d9f97af714ef8
Fatih Yüksel <fatihyuksel3109@gmail.com>
2023-11-23 13:36:06 +0300

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+// This is function to determine if a word is palindrome or not.
+//It returns "The word ..." is a palindrome if it is true and returns
+// "The word ..." is not a palindrome
+
+
+function isPalindrome(str) {
+
+    let reverseStr = str.toLowerCase().split('').reverse().join("");
+
+    if (reverseStr === str.toLowerCase()) {
+        return `The word ${str} is a palindrome`;
+    } else {
+        return `The word ${str} is not a palindrome`;
+    }
+
+}
+
+console.log(isPalindrome("AKka"));
