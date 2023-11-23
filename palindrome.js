4117f83c59373d9ae694b4d831d7f1a960f30d3f
Fatih Yüksel <fatihyuksel3109@gmail.com>
2023-11-23 13:46:08 +0300

    palindrome update

--- palindrome.js
+++ palindrome.js
@@ -1,18 +1,19 @@
-// This is function to determine if a word is palindrome or not.
-//It returns "The word ..." is a palindrome if it is true and returns
-// "The word ..." is not a palindrome if it is not
-
+// This is a function to determine if a word is a palindrome or not.
+// It takes a string as input and returns a message indicating whether the word is a palindrome or not.
+// If the word is a palindrome, it returns "The word ... is a palindrome."
+// If the word is not a palindrome, it returns "The word ... is not a palindrome."
 
 function isPalindrome(str) {
-
+    // Convert the input string to lowercase to ensure a case-insensitive comparison
     let reverseStr = str.toLowerCase().split('').reverse().join("");
 
+    // Check if the reversed string is equal to the original string
     if (reverseStr === str.toLowerCase()) {
         return `The word ${str} is a palindrome`;
     } else {
         return `The word ${str} is not a palindrome`;
     }
-
 }
 
+// Test the function with a sample case
 console.log(isPalindrome("AKka"));
