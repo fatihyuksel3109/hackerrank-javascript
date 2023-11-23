b22ef36aba4dfb03da3c6c3d14ed71be23f7a8ec
Fatih Yüksel <fatihyuksel3109@gmail.com>
2023-11-23 13:37:58 +0300

    palindrome update

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
 // This is function to determine if a word is palindrome or not.
 //It returns "The word ..." is a palindrome if it is true and returns
-// "The word ..." is not a palindrome
+// "The word ..." is not a palindrome if it is not
 
 
 function isPalindrome(str) {
